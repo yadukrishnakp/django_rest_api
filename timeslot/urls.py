f9c1f18e90983d4a7baaaec1e0b38f338c9d5ea7
yadukrishnakp <yadukrishnakp007@gmail.com>
2022-03-07 10:16:55 +0530

    now admin can check slots between candidates

--- timeslot/urls.py
+++ timeslot/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.RegisterList, name="register_list"),
-    path('user_register/', views.User_Register, name="user_register")
+    path('user_register/', views.User_Register, name="user_register"),
+    path('time_slots/<str:inter_id>/<str:cand_id>/', views.TimeSlots, name="time_slots")
 ]
--- timeslot/views.py
+++ timeslot/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegisterSerializers
 from .models import Register
+from datetime import datetime
 
 
 # Create your views here.
@@ -23,3 +21,23 @@ def User_Register(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def TimeSlots(request, inter_id, cand_id):
+    data1 = Register.objects.get(id=inter_id)
+    data2 = Register.objects.get(id=cand_id)
+    serializer1 = RegisterSerializers(data1, many=False)
+    serializer2 = RegisterSerializers(data2, many=False)
+    date1 = datetime.strptime(serializer1.data['time_from'], '%H:%M:%S').time()
+    date2 = datetime.strptime(serializer1.data['time_to'], '%H:%M:%S').time()
+    date3 = datetime.strptime(serializer2.data['time_from'], '%H:%M:%S').time()
+    date4 = datetime.strptime(serializer2.data['time_to'], '%H:%M:%S').time()
+    list1 = []
+    for i in range(max(date1, date3).hour, min(date2, date4).hour):
+        list1.append((i, i + 1))
+
+    if not list1:
+        return Response("you don't have slots for this candidate")
+    else:
+        return Response(list1)
